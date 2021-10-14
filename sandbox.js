6a58411e975b8609fd4d751ff63ec4a75faf8c8a
LTsandbox1 <sandbox1@bttcloud.com>
2021-10-14 10:15:41 +0300

    Update sandbox.js
    fixed port constant

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = 80
 var os = require("os");
-app.listen(5000)
+app.listen(port)
 app.get('', (req, res) => {res.send((os.hostname()))})
