caf9356c0b347f5bcb4151bdf59494f05a380948
Oleksandr Bryndzii <v5okc7@gmail.com>
2021-12-14 23:27:05 +0200

    Add Dockerfile changed port constant to 8080

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,9 @@
+'use strict';
 const express = require('express')
 const app = express()
-const port = 80
+const port = 8080
+const host = '0.0.0.0';
 var os = require("os");
 app.listen(port)
 app.get('', (req, res) => {res.send((os.hostname()))})
+console.log(`Running on http://${host}:${port}`);
